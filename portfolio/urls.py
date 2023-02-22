e0b697d544f2bdba713c5f8a63cf981fefeeeb67
puskarjoshi22@gmail.com <puskarjoshi22@gmail.com>
2023-02-23 00:23:47 +0545

    projects section fully working

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,9 +1,11 @@
 from urllib.parse import urlparse
 from django.urls import path
-from .views import portfolio
+from .views import portfolio, show_all_projects, project_detail
 
 app_name = 'portfolio'
 
 urlpatterns = [
     path('', portfolio, name='portfolio'),
+    path('projects/', show_all_projects, name='show_all_projects'),
+    path('projects/<int:project_id>', project_detail, name='project_detail')
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Project, PersonalInformation
 
@@ -10,3 +10,15 @@ def portfolio(request):
     return render(request, 'portfolio/portfolio.html', {'projects': projects, 'personal_info': personal_info})
 
 
+def show_all_projects(request):
+    projects = Project.objects.all()
+    context = {'projects':projects}
+    return render(request, 'portfolio/show_all_projects.html', context)
+
+
+def project_detail(request, project_id):
+    project = get_object_or_404(Project, pk=project_id)
+    context = {
+        'project': project
+    }
+    return render(request, 'portfolio/project_detail.html', context)
