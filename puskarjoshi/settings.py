050152b4393667302b565c62ece5c0d303bef67d
puskarjoshi22@gmail.com <puskarjoshi22@gmail.com>
2023-02-25 18:30:02 +0545

    debug issue

--- puskarjoshi/settings.py
+++ puskarjoshi/settings.py
@@ -21,7 +21,7 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = False
 
 try:
     from .local_settings import *
