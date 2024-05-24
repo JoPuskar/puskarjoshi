2a7be63755a9fece9eb55d4030c6d54111ed46e3
JoPuskar <joship1@etsu.edu>
2024-05-24 09:19:56 -0400

    allowed host updated

--- puskarjoshi/settings.py
+++ puskarjoshi/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # load_dotenv(BASE_DIR / '.env')
 
-ALLOWED_HOSTS = ['puskarjoshi.com.np']
+ALLOWED_HOSTS = ['puskarjoshi.com.np', 'www.puskarjoshi.com.np', 'localhost', '3.21.113.166', '127.0.0.1']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
